561300abdc8297457b305882643a72cc8160ffe8
Yashvi <ysiddhapura@mail.csuchico.edu>
2023-04-19 12:45:05 -0700

    Updated comments for detecting labels

--- lines/lines.cpp
+++ lines/lines.cpp
@@ -35,14 +35,6 @@ int main(int argc, char** argv)
    int waitval=100;
    bool origView=1,tview=1;
 
-/*
-   VideoWriter vout;
-    Size S = Size((int) cap.get(CAP_PROP_FRAME_WIDTH),    // Acquire input size
-                  (int) cap.get(CAP_PROP_FRAME_HEIGHT));
-
-   vout.open("lines.mp4", VideoWriter::fourcc('H','2','6','4'), cap.get(CAP_PROP_FPS), S, true);
-*/
-
    printf("Press space to pause/unpause, 's' to step frame by frame, 'c' to switch views or 'esc' to exit\n");
    int cnt = 0;
    vector<vector<Vec4i>> linehistory;
@@ -55,7 +47,7 @@ int main(int argc, char** argv)
       if(src.empty()){
         break;
       }
-      //store mat variables
+
       Mat dst,gray;
       Mat gray1,gray2,gray3;
 
@@ -63,143 +55,113 @@ int main(int argc, char** argv)
        int gval = 0,bval=0,rval=0;
        int myhist[256] = {0};
 
-       //to focus on just the mousepad area
-       roisrc=src(Rect(ROIX,ROIY,SRCX-ROIX,SRCY-ROIY));
-//      GaussianBlur(src,gray, Size(81,81), 3);
-//       bilateralFilter(src,gray,4,8,2);
 
-       for(int j=0;j<src.rows-2;j++)
-       {
-//         int scandown = 0;
-         for(int i=0;i<src.cols;i++)
-         {
-             bval = gray.at<Vec3b>(j,i)[0];
-             gval = gray.at<Vec3b>(j,i)[1];
-             rval = gray.at<Vec3b>(j,i)[2];
-//             bval=bval+(8-bval%8);gval=gval+(8-gval%8);rval=rval+(8-rval%8);
-             if(abs(bval-gval)>10 || abs(rval-gval)>10 || abs(rval-bval)>10) {gval=0;}
-             else 
-             {
-//                     scandown--;
-//                    if(gval>240){scandown=50;}
-//                    if(scandown<0) {gval=0;}
-//                    gval=(gval>150)?gval:255;
+//to add:  change roix and roiy values to adapt to new video set
+//most likely need to use something like this
+      roisrc = src(Rect(0,0,780,959));
 
+      //roisrc=src(Rect(ROIX,ROIY,SRCX-ROIX,SRCY-ROIY));
 
-//                  gval=gval+(64-gval%64);
-
-
-//                  gval=int((float)gval*((float)gval/255));
-
-//                  if(gval>255){gval=255;}           
-//                  if(gval>64&&gval<192){gval=0;}       
-
-
-//                  gval=(gval<50)?0:(gval<200)?128:(gval<225)?196:255;
-//                  gval=(gval<100)?0:((gval>240)?255:gval/2);                 
-             }
-             myhist[gval]++;
-//             gray.at<Vec3b>(j,i)[0]=gval;
-//             gray.at<Vec3b>(j,i)[1]=gval;
-//             gray.at<Vec3b>(j,i)[2]=gval;
-         }
-       }
-
-//       bilateralFilter(gray,dst,16,32,8);
-
-//      GaussianBlur(gray,src, Size(9,9), 3);
-//      addWeighted(gray,1.5,src,-0.5,0,src);
-//      gray.convertTo(gray,-1,1,-200);
-//      gray.convertTo(gray,-1,2,0);
-//      gray.convertTo(gray,-1,1,-50);
-//      gray.convertTo(gray,-1,2,0);
-//      gray.convertTo(gray,-1,1,-50);
-//      gray.convertTo(gray,-1,2,0);
-//      gray.convertTo(gray,-1,1,-100);
-//      gray.convertTo(gray,-1,1.5,0);
-
-//        dst = gray.clone();
-
-//      gray = src.clone();
-//      threshold(gray,dst,150,255,THRESH_BINARY);
-//dst = gray.clone();
-//      GaussianBlur(gray, gray, Size(9,9), 2, 2);
-
-
-      // Edge detection with thresholds of param3 to param4 intensity for hysteresis with param5xparam5 sobel and default l1 norm 
-
-
-//      Canny(gray, dst, 100, 100, 3);
-//      cv::cvtColor(src, gray, COLOR_BGR2GRAY);
       Canny(roisrc, dst, 1, 225, 3);
-
-
-
+     
       vector<Vec4i> linesP; // will hold the results of the detection
-      vector<Vec4i> parallels; //will store the parallel lines
+      
+      vector<Vec4i> parallels;
+      
       HoughLinesP(dst, linesP, 1, CV_PI/180, 20, 75, 10 ); // runs the actual detection
+      
+/*
+
+Options:
+
+Instead of parallel lines, look for a specific slope to determine if line is horizontal as new videos will have syringes oriented that way
+
+In addition to minimum line length retrieved from houghlinesp parameter, look for a specific maximum line by adding a condition where x2-x1 is in range
+i.e. if min length from parameter is 75 but we do not want lines longer than 100 then add a if(l[2]-l[0] < 100) to only pick up lines of length 75-100
+
+
+to differentiate labels, we need to look for lines that are a given height between each other
+
+first identify top most line
+
+then as looping through lines find the next line that has l[3] - l[1] (height, y value differences) greater than threshold
+for 1280x960 typical syringe height is probably 100-120 or so height
+
+once the set of top label lines are identified, draw bounding box
+
+
+*/
 
       cout << "Lines from new Frame:" << endl;
-      
-      //for loop to go through the linesP vector
+      //go through the linesP vector 
       for( size_t i = 0; i < linesP.size(); i++ )
       {
-        //calculating the slope
+
+//to add: first job is to ignore lines too wide l[2]-l[0] > 100 or something like that to identify label lines
+		//slope is equal to 0
         float slope=0;
-        //flag to detect if parallel or not
+        //flag for parallel lines
         bool hasparallel=0;
-        //Store each vector element 
+        //store the x1, x2, y1, y2 coordinate points
         Vec4i l = linesP[i];
-        //prints the x1, x2, y1, y2 coordinate 
+        //output the coordinates
         cout << l[0] << " " << l[1] << " " << l[2] << " " << l[3] << endl;
-        //calculate the slop y2-y1, x2 -x1
+        //calculate the slope for (y2-y1)/(x2-x1)
         slope = (float)(l[3]-l[1])/(float)(l[2]-l[0]);
-        //print the slope
+        //output the slope
+// to add:  check if slope is approximately horiztonal, only continue if it is otherwise skip line
+// also to add:  keep track of minimum y (top most line)
+//the code below can be removed as we do not need to look for parallel lines if we are thresholding the y gap between lines
+//keep track with ymin variable
         cout << slope <<endl;
-        //go through the second line detected
+        //for loop through the linesP vector for the second line
         for( size_t j = 0; j < linesP.size(); j++ )
         {
-          //store the vector element for the second line
+		    //store the coordinas for the second line
            Vec4i l2 = linesP[j];
-           //calcylate the slope (y2-y1)/(x2-x1)
+           //calculating the slope
            float slope2 = (float)(l2[3]-l2[1])/(float)(l2[2]-l2[0]);
-           //checking againest the first slope
+           //checking if the slope is similar to slope1
            cout << "checking against slope "<<slope2<<endl;
-           //finding the difference of the slopes of both lines
+           //calculating the slope difference
            float slopediff = abs(slope-slope2);
-           //if the difference is less than 0.3 than it is parallel
+           //outputting the difference
            cout<< "Difference #" << j << " " << slopediff << endl;
-           //update the hasparrallel flag to 1
+           //if condition is met, and slope is less than 0.3, then we change the flag to parallel
            if(slopediff <0.3){cout<<"Parallel line found " << endl;hasparallel=1;}
         }
-        //if it is parallel, then we display the lines, and add it to the parallel vector
+        //if parallel, then we display the line, takng into account the area we want it to detcect
         if(hasparallel){
         line( src, Point(l[0]+ROIX, l[1]+ROIY), Point(l[2]+ROIX, l[3]+ROIY), Scalar(0,0,255), 3, LINE_AA);
+        //add to the parrallel vector
         parallels.push_back(linesP[i]);
         }
+
+//to add:  after identifying the top most line of length label
+//  
       }
-      //otherwise we push it to the line history vector
+      //if parallel vector size is greater than 0
       if(parallels.size()>0){
+		  //add to the linehistory vector
       linehistory.push_back(parallels);
       }
-      //will print the line history elements
+
+cout << "test test test " << endl;
+
+/*      
       cout << "Line History " << endl;
+      
       for(int x=linehistory.size();x>((linehistory.size()>10)?linehistory.size()-10:0);x--){
-        //for each frame number
+		 //outputtng frame #
         cout << "frame #" << x << endl;
-
         vector<Vec4i> linesP = linehistory[x-1];
-
         cout << linesP.size() << endl;
-
         for( size_t i = 0; i < linesP.size(); i++ ) {
-
           Vec4i l = linesP[i];
-
           cout << l[0] << " " << l[1] << " " << l[2] << " " << l[3] << endl;
         }
       }
-
+*/
       if (origView)
       {
         imshow(filename, src);
