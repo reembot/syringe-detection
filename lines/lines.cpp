37f48dce4a2f839b80385cefc123704748cedd95
Yashvi <ysiddhapura@mail.csuchico.edu>
2023-04-26 10:39:42 -0700

    Drawing boxes

--- lines/lines.cpp
+++ lines/lines.cpp
@@ -1,17 +1,11 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 using namespace std;
 
-// See www.asciitable.com
 #define ESCAPE_KEY (27)
 #define SYSTEM_ERROR (-1)
 #define ROIX (300)
@@ -21,169 +15,105 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-   const char* default_file = "0";
-   const char* filename = argc >=2 ? argv[1] : default_file;
-   VideoCapture cap;
-   if (*filename == '0') {
-     cap.open(0);
-   } else {
-     cap.open(filename);
-   }
-   bool canView = 0;
-   namedWindow(filename);
-   char winInput;
-   int waitval=100;
-   bool origView=1,tview=1;
+    const char* default_file = "0";
+    const char* filename = argc >= 2 ? argv[1] : default_file;
+    VideoCapture cap;
+    if (*filename == '0') {
+        cap.open(0);
+    } else {
+        cap.open(filename);
+    }
+    bool canView = 0;
+    namedWindow(filename);
+    char winInput;
+    int waitval = 100;
+    bool origView = 1, tview = 1;
 
-   printf("Press space to pause/unpause, 's' to step frame by frame, 'c' to switch views or 'esc' to exit\n");
-   int cnt = 0;
-   vector<vector<Vec4i>> linehistory;
-   while (1)
-   {
-      Mat src,roisrc;
-      cnt++;
-      
-      cap.read(src);
-      if(src.empty()){
-        break;
-      }
+    printf("Press space to pause/unpause, 's' to step frame by frame, 'c' to switch views or 'esc' to exit\n");
+    int cnt = 0;
+    vector<vector<Vec4i>> linehistory;
+    while (1)
+    {
+        Mat src, roisrc;
+        cnt++;
 
-      Mat dst,gray;
-      Mat gray1,gray2,gray3;
+        cap.read(src);
+        if (src.empty()) {
+            break;
+        }
 
-       gray = src.clone();
-       int gval = 0,bval=0,rval=0;
-       int myhist[256] = {0};
+        Mat dst;
+       
+        int myhist[256] = {0};
 
+        roisrc = src(Rect(0, 0, 780, 959));
 
-//to add:  change roix and roiy values to adapt to new video set
-//most likely need to use something like this
-      roisrc = src(Rect(0,0,780,959));
+        Canny(roisrc, dst, 1, 225, 3);
 
-      //roisrc=src(Rect(ROIX,ROIY,SRCX-ROIX,SRCY-ROIY));
+        vector<Vec4i> linesP;
 
-      Canny(roisrc, dst, 1, 225, 3);
-     
-      vector<Vec4i> linesP; // will hold the results of the detection
-      
-      vector<Vec4i> parallels;
-      
-      HoughLinesP(dst, linesP, 1, CV_PI/180, 20, 75, 10 ); // runs the actual detection
-      
-/*
+        HoughLinesP(dst, linesP, 1, CV_PI / 180, 20, 75, 10);
 
-Options:
+        cout << "Lines from new Frame:" << endl;
 
-Instead of parallel lines, look for a specific slope to determine if line is horizontal as new videos will have syringes oriented that way
+        int ymin = INT_MAX;
+        Vec4i top_line;
 
-In addition to minimum line length retrieved from houghlinesp parameter, look for a specific maximum line by adding a condition where x2-x1 is in range
-i.e. if min length from parameter is 75 but we do not want lines longer than 100 then add a if(l[2]-l[0] < 100) to only pick up lines of length 75-100
-
-
-to differentiate labels, we need to look for lines that are a given height between each other
-
-first identify top most line
-
-then as looping through lines find the next line that has l[3] - l[1] (height, y value differences) greater than threshold
-for 1280x960 typical syringe height is probably 100-120 or so height
-
-once the set of top label lines are identified, draw bounding box
-
-
-*/
-
-      cout << "Lines from new Frame:" << endl;
-      //go through the linesP vector 
-      for( size_t i = 0; i < linesP.size(); i++ )
-      {
-
-//to add: first job is to ignore lines too wide l[2]-l[0] > 100 or something like that to identify label lines
-		//slope is equal to 0
-        float slope=0;
-        //flag for parallel lines
-        bool hasparallel=0;
-        //store the x1, x2, y1, y2 coordinate points
-        Vec4i l = linesP[i];
-        //output the coordinates
-        cout << l[0] << " " << l[1] << " " << l[2] << " " << l[3] << endl;
-        //calculate the slope for (y2-y1)/(x2-x1)
-        slope = (float)(l[3]-l[1])/(float)(l[2]-l[0]);
-        //output the slope
-// to add:  check if slope is approximately horiztonal, only continue if it is otherwise skip line
-// also to add:  keep track of minimum y (top most line)
-//the code below can be removed as we do not need to look for parallel lines if we are thresholding the y gap between lines
-//keep track with ymin variable
-        cout << slope <<endl;
-        //for loop through the linesP vector for the second line
-        for( size_t j = 0; j < linesP.size(); j++ )
+        for (size_t i = 0; i < linesP.size(); i++)
         {
-		    //store the coordinas for the second line
-           Vec4i l2 = linesP[j];
-           //calculating the slope
-           float slope2 = (float)(l2[3]-l2[1])/(float)(l2[2]-l2[0]);
-           //checking if the slope is similar to slope1
-           cout << "checking against slope "<<slope2<<endl;
-           //calculating the slope difference
-           float slopediff = abs(slope-slope2);
-           //outputting the difference
-           cout<< "Difference #" << j << " " << slopediff << endl;
-           //if condition is met, and slope is less than 0.3, then we change the flag to parallel
-           if(slopediff <0.3){cout<<"Parallel line found " << endl;hasparallel=1;}
-        }
-        //if parallel, then we display the line, takng into account the area we want it to detcect
-        if(hasparallel){
-        line( src, Point(l[0]+ROIX, l[1]+ROIY), Point(l[2]+ROIX, l[3]+ROIY), Scalar(0,0,255), 3, LINE_AA);
-        //add to the parrallel vector
-        parallels.push_back(linesP[i]);
+            Vec4i l = linesP[i];
+            float slope = (float)(l[3] - l[1]) / (float)(l[2] - l[0]);
+
+            // Check if the line is approximately horizontal and has a length between 75 and 100
+            if (abs(slope) < 0.3 && (l[2] - l[0] >= 75) && (l[2] - l[0] <= 100))
+            {
+                // Check if the line is the topmost line
+                if (l[1] < ymin)
+                {
+                    ymin = l[1];
+                    top_line = l;
+                }
+            }
         }
 
-//to add:  after identifying the top most line of length label
-//  
-      }
-      //if parallel vector size is greater than 0
-      if(parallels.size()>0){
-		  //add to the linehistory vector
-      linehistory.push_back(parallels);
-      }
+        // Draw the topmost line
+        line(src, Point(top_line[0] + ROIX, top_line[1] + ROIY), Point(top_line[2] + ROIX, top_line[3] + ROIY), Scalar(0, 0, 255), 3, LINE_AA);
 
-cout << "test test test " << endl;
+        // Calculate the bounding box coordinates based on the topmost line
+        int box_width = top_line[2] - top_line[0];
+        int box_height = box_width * 2; // Assuming the height is twice the width
+        Rect bounding_box(top_line[0] + ROIX, top_line[1] + ROIY, box_width, box_height);
 
-/*      
-      cout << "Line History " << endl;
-      
-      for(int x=linehistory.size();x>((linehistory.size()>10)?linehistory.size()-10:0);x--){
-		 //outputtng frame #
-        cout << "frame #" << x << endl;
-        vector<Vec4i> linesP = linehistory[x-1];
-        cout << linesP.size() << endl;
-        for( size_t i = 0; i < linesP.size(); i++ ) {
-          Vec4i l = linesP[i];
-          cout << l[0] << " " << l[1] << " " << l[2] << " " << l[3] << endl;
+        // Draw the bounding box
+        rectangle(src, bounding_box, Scalar(0, 255, 0), 2);
+
+        if (origView)
+        {
+            imshow(filename, src);
         }
-      }
-*/
-      if (origView)
-      {
-        imshow(filename, src);
-      } else if(tview) {
-        imshow(filename,gray);
-//      } else { 
-//        imshow(filename,dst);
-      }
-//      vout.write(src);
+        else if (tview)
+        {
+            imshow(filename, dst);
+        }
 
-      if ((winInput = waitKey(waitval)) == ESCAPE_KEY)
-      {
-          break;
-      }
-      else if(winInput == 's') 
-      {
-          waitval = 0;
-      }
-      else if(winInput == 'h') 
-      {
-          int total=0;
-          for(int i=0;i<256;i++){if(myhist[i]>0){cout<<i<<" "<<myhist[i]<<endl;total+=myhist[i];}}
+        if ((winInput = waitKey(waitval)) == ESCAPE_KEY)
+        {
+            break;
+        }
+        else if (winInput == 's')
+        {
+            waitval = 0;
+        }
+        else if (winInput == 'h')
+        {
+            int total = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                if (myhist[i] > 0)
+                {
+                    cout << i << " " << myhist[i]<<endl;
+                    
+                    total+=myhist[i];}}
       }
       else if(winInput == ' ') 
       {
