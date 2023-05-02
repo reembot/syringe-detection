7d4b68152fa2f003b4d8167967b665ed661d79a3
toferwon <kselvidge@mail.csuchico.edu>
2023-05-01 20:46:29 -0700

    Added ocr updated utils for scorestamps and first label header

--- utils/colorhist.cpp
+++ utils/colorhist.cpp
@@ -22,14 +22,20 @@ int main(int argc, char** argv)
    const char* default_file = "0";
    const char* output_file = "vout.csv";
    const char* filename = argc >=2 ? argv[1] : default_file;
-   String out_filename=filename, out4_filename=filename, outper_filename=filename;
+   String out_filename=filename, out8_filename=filename, outper_filename=filename,outmm_filename=filename,outmmper_filename=filename;
    out_filename += ".colorhist.csv";
-   out4_filename += ".color4hist.csv";
+   out8_filename += ".color8hist.csv";
    outper_filename += ".colorperhist.csv";
-   ofstream outfile,out4file,outperfile;
+   outmm_filename += ".colormmhist.csv";
+   outmmper_filename += ".colormmperhist.csv";
+   ofstream outfile,out4file,outperfile,outmmfile,outmmperfile;
+/*
    outfile.open(out_filename);
-   out4file.open(out4_filename);
+   out4file.open(out8_filename);
    outperfile.open(outper_filename);
+   outmmfile.open(outmm_filename);
+*/
+   outmmperfile.open(outmmper_filename);
    VideoCapture cap;
    int waitval = 0;
    if (*filename == '0') {
@@ -41,40 +47,79 @@ int main(int argc, char** argv)
    char winInput;
    int cnt=0;
 
-   Mat src;
+   Mat presize,src;
 
 //   printf("Press space to pause and unpause, 's' to step through each frame, or 'esc' to exit\n");
 
    while (1)
    {
-      cap.read(src);
-      if(src.empty())
+      cap.read(presize);
+      if(presize.empty())
       {
         break;
       }
+      resize(presize, src, Size(175,50), INTER_LINEAR);
       cnt++;
+      int mmhist[512] = {0};
+      int bval=0,gval=0,rval=0,b8val=0,g8val=0,r8val=0,mmval=0;
       int bhist[256] = {0}, ghist[256] = {0}, rhist[256] = {0};
-      int b4hist[4] = {0}, g4hist[4] = {0}, r4hist[4] = {0};
+      int b8hist[8] = {0}, g8hist[8] = {0}, r8hist[8] = {0};
+      int blo=0,bhi=0,glo=0,ghi=0,rlo=0,rhi=0;
       for(int j=0;j<src.rows;j++)
       {
         for(int i=0;i<src.cols;i++)
         {
-           bhist[src.at<Vec3b>(j,i)[0]]++;
-           if(src.at<Vec3b>(j,i)[0]<64){b4hist[0]++;}else if(src.at<Vec3b>(j,i)[0]<128){b4hist[1]++;}else if(src.at<Vec3b>(j,i)[0]<192){b4hist[2]++;}else{b4hist[3]++;}
+           bval = src.at<Vec3b>(j,i)[0];           
+           gval = src.at<Vec3b>(j,i)[1];           
+           rval = src.at<Vec3b>(j,i)[2];           
+           b8val = bval / 32;
+           g8val = gval / 32;
+           r8val = rval / 32;
+           bhist[bval]++;
+           ghist[gval]++;
+           rhist[rval]++;
+           b8hist[b8val]++;
+           g8hist[g8val]++;
+           r8hist[r8val]++;
+           mmval=b8val*64+g8val*8+r8val;           
+           mmhist[mmval]++;
+/*           if(src.at<Vec3b>(j,i)[0]<32){b8hist[0]++;}else if(src.at<Vec3b>(j,i)[0]<64){b8hist[1]++;}else if(src.at<Vec3b>(j,i)[0]<96){b8hist[2]++;}else if(src.at<Vec3b>(j,i)[0]<128){b8hist[3]++;}
+           else if(src.at<Vec3b>(j,i)[0]<160){b8hist[4]++;}else if(src.at<Vec3b>(j,i)[0]<192){b8hist[5]++;}else if(src.at<Vec3b>(j,i)[0]<224){b8hist[6]++;}else{b8hist[7]++;}
            ghist[src.at<Vec3b>(j,i)[1]]++;
-           if(src.at<Vec3b>(j,i)[1]<64){g4hist[0]++;}else if(src.at<Vec3b>(j,i)[1]<128){g4hist[1]++;}else if(src.at<Vec3b>(j,i)[1]<192){g4hist[2]++;}else{g4hist[3]++;}
+           if(src.at<Vec3b>(j,i)[1]<32){g8hist[0]++;}else if(src.at<Vec3b>(j,i)[1]<64){g8hist[1]++;}else if(src.at<Vec3b>(j,i)[1]<96){g8hist[2]++;}else if(src.at<Vec3b>(j,i)[1]<128){g8hist[3]++;}
+           else if(src.at<Vec3b>(j,i)[1]<160){g8hist[4]++;}else if(src.at<Vec3b>(j,i)[1]<192){g8hist[5]++;}else if(src.at<Vec3b>(j,i)[1]<224){g8hist[6]++;}else{g8hist[7]++;}
            rhist[src.at<Vec3b>(j,i)[2]]++;
-           if(src.at<Vec3b>(j,i)[2]<64){r4hist[0]++;}else if(src.at<Vec3b>(j,i)[2]<128){r4hist[1]++;}else if(src.at<Vec3b>(j,i)[2]<192){r4hist[2]++;}else{r4hist[3]++;}
+           if(src.at<Vec3b>(j,i)[2]<32){r8hist[0]++;}else if(src.at<Vec3b>(j,i)[2]<64){r8hist[1]++;}else if(src.at<Vec3b>(j,i)[2]<96){r8hist[2]++;}else if(src.at<Vec3b>(j,i)[2]<128){r8hist[3]++;}
+           else if(src.at<Vec3b>(j,i)[2]<160){r8hist[4]++;}else if(src.at<Vec3b>(j,i)[2]<192){r8hist[5]++;}else if(src.at<Vec3b>(j,i)[2]<224){r8hist[6]++;}else{r8hist[7]++;}
+*/
         }
       }
       for(int i=0;i<256;i++){
-      outfile << cnt << "," << i << "," << bhist[i] << "," << ghist[i] << "," << rhist[i] << endl;
+//      outfile << cnt << "," << i << "," << bhist[i] << "," << ghist[i] << "," << rhist[i] << endl;
       }
       int totalp = src.rows*src.cols;
-      for(int i=0;i<4;i++){
-      out4file << cnt << "," << i << "," << b4hist[i] << "," << g4hist[i] << "," << r4hist[i] << endl;
-      outperfile << cnt << "," << i << "," << to_string((float)b4hist[i]/totalp) << "," << to_string((float)g4hist[i]/totalp) << "," << to_string((float)r4hist[i]/totalp) << endl;
+      for(int i=0;i<8;i++){
+//        out4file << cnt << "," << i << "," << b8hist[i] << "," << g8hist[i] << "," << r8hist[i] << endl;
+//        outperfile << cnt << "," << i << "," << to_string((float)b8hist[i]/totalp) << "," << to_string((float)g8hist[i]/totalp) << "," << to_string((float)r8hist[i]/totalp) << endl;
       }
+      for(int i=0;i<512;i++){
+        float mmper = (float)mmhist[i]/totalp;
+        if(mmper > 0.02) {
+          bval = i / 64;
+          gval = (i % 64) / 8;
+          rval = i % 8;
+          blo = bval * 32;
+          bhi = (bval + 1) * 32;
+          glo = gval * 32;
+          ghi = (gval + 1) * 32;
+          rlo = rval * 32;
+          rhi = (rval + 1) * 32;           
+//          outmmfile << cnt << ",(B " << blo << "-" << bhi << " G " << glo << "-" << ghi << " R " << rlo << "-" << rhi << ")," << mmhist[i] << endl;
+          outmmperfile << cnt << ",(B " << blo << "-" << bhi << " G " << glo << "-" << ghi << " R " << rlo << "-" << rhi << ")," << to_string((float)mmhist[i]/totalp) << endl;
+        }
+      }
+
+
       imshow(filename,src);
 
       if ((winInput = waitKey(waitval)) == ESCAPE_KEY)
@@ -96,4 +141,8 @@ int main(int argc, char** argv)
    outfile.close();
    out4file.close();
    outperfile.close();
+   outmmfile.close();
+   outmmperfile.close();
+
+
 }
